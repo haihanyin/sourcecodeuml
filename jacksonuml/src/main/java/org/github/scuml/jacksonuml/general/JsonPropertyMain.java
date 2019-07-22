0584bb94173482ad2e0d332959776fdab228b1e5
haihanyin <iamhaihan@gmail.com>
2019-07-22 22:28:49 +0200

    add code analysis

--- jacksonuml/src/main/java/org/github/scuml/jacksonuml/general/JsonPropertyMain.java
+++ jacksonuml/src/main/java/org/github/scuml/jacksonuml/general/JsonPropertyMain.java
@@ -4,15 +4,48 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * - How is a field serialized by its getter?
+ * <p>
+ * Call getter by using reflection in BeanPropertyWriter.serializeAsField (ln688)
+ * <p>
+ * BeanPropertyWriter originates from BeanSerializerFactory (ln368)
+ * List<BeanPropertyWriter> props = findBeanProperties(prov, beanDesc, builder);
+ * <p>
+ * The bean description contains all the metadata of the bean, where the property information
+ * is retrieved.
+ * The bean description is initialized in BeanSerializerFactory (ln135)
+ * BeanDescription beanDesc = config.introspect(origType);
+ * <p>
+ * origType is derived from the type of the bean class
+ * <p>
+ * - How the @JsonProperty is read in serialization?
+ * <p>
+ * The field name is written by BeanPropertyWriter, which is used earlier for calling the getter
+ * gen.writeFieldName(_name); (ln725)
+ * <p>
+ *     The _name originates from a POJOPropertyBuilder, in which _name represents the string for
+ *     serialization and _internalName represents the actual field name
+ * <p>
+ *     the name value originates from POJOPropertyBuilder.findExplicitNames
+ *     the POJOProertyBuilder contains information about the fields, getters, setters related to
+ *     a property etc
+ * <p>
+ *     the getters are retrieved by calling
+ *     AnnotatedMember valueAccessor = beanDesc.findJsonValueAccessor();
+ *     in BasicSerializerFactory (ln346)
+ * ??
+ *
+ *
+ */
 public class JsonPropertyMain {
 
     public static void main(String[] args) throws JsonProcessingException {
         MyBean bean = new MyBean(1, "My bean");
-
         String result = new ObjectMapper().writeValueAsString(bean);
-
         System.out.println(result);
     }
+
     public static class MyBean {
         public int id;
         private String name;
@@ -22,13 +55,13 @@ public class JsonPropertyMain {
             this.name = name;
         }
 
-        @JsonProperty("name")
-        public void setTheName(String name) {
+        @JsonProperty("thename")
+        public void setName(String name) {
             this.name = name;
         }
 
-        @JsonProperty("name")
-        public String getTheName() {
+        @JsonProperty("thename")
+        public String getName() {
             return name;
         }
     }
